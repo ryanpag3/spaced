f212c3d09b1e68d6c36cba843bbbcc35089b27e8
Ryan Page <ryanapage@gmail.com>
2025-04-02 23:07:39 -0700

    #1 start writing out mobile crypto library

--- cli/src/cmd/init.ts
+++
@@ -1,5 +0,0 @@
-import { isInitialized } from '../lib/command';
-
-export default async function initialize() {
-
-}
--- crypto/src/Crypto.ts
+++
@@ -1,6 +0,0 @@
-export interface Crypto {
-    /**
-     * Generate a key encryption key from a password.
-     */
-    getKEK: (password: string) => Promise<Buffer>;
-}
